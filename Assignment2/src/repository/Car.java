98d888da33dbcdc549d989edff3e0ea438768c55
fh25 <fhj@fhjs-macbook.wpa.txstate.edu>
2014-09-17 12:16:11 -0500

    test

--- Assignment2/src/repository/Car.java
+++ Assignment2/src/repository/Car.java
@@ -10,7 +10,7 @@ package repository;
  *
  * @author fhj
  */
-public class Car extends Vehicle { //this is a test
+public class Car extends Vehicle { //this is a test, asdfkljsdflkjsalkf
   
   /**
    * This car's body style.
